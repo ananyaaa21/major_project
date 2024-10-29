c5e8edbe478feb631e12bd2bf730e05f9230a7e9
ananyaaa21 <132597794+ananyaaa21@users.noreply.github.com>
2024-10-29 22:31:03 +0530

    Update auth0Config.js

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth ({
-    audience: "http://localhost:8000",
+    audience: "https://realestate-frontend-black.vercel.app/",
     issuerBaseURL: "https://dev-fopwyk5gbf15lxbe.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
